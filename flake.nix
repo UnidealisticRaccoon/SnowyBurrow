1ccaeba5cd58564b19fc120c255a9db32041bc70
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-04 18:43:04 -0300

    feat!: remove hello package

--- flake.nix
+++ flake.nix
@@ -11,10 +11,6 @@
 
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        packages.default = pkgs.hello;
-      };
-
       flake = { };
     };
 }
