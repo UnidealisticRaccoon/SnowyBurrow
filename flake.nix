637c1217ddbb6491366de5c332ba5c3c42e62861
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-04 20:52:32 -0300

    feat: setup binary cache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,19 @@
 {
   description = "Personal Nix Config: powered by flake-parts";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.garnix.io"
+      "https://procyon.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "procyon.cachix.org-1:VljSnjtMCpriTsvji4EotHS9UZJoPMOCVk//fmmvIto="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
