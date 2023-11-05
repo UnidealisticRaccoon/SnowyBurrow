06323c5b865fa1f2b45f93fde550ee67bf1bcfd6
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-05 18:28:14 -0300

    feat: bootstrap reuse compliance

--- nix/flake/formatter/default.nix
+++ nix/flake/formatter/default.nix
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Lin Yinfeng <lin.yinfeng@outlook.com>
+# SPDX-FileCopyrightText: 2023 Unidealistic Raccoon <procyon@secureninja.maskmy.id>
+#
+# SPDX-License-Identifier: MIT
+
 { inputs, ... }: {
   imports = [
     inputs.flake-root.flakeModule
