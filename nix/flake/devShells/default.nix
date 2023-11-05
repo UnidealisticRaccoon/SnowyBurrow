797bd02cb3f588aa93dd913ede6f8c7ce26bf138
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-05 11:39:57 -0300

    feat(formatter): setup treefmt

--- nix/flake/devShells/default.nix
+++ nix/flake/devShells/default.nix
@@ -6,6 +6,13 @@
       shellHook = ''
         source ${config.pre-commit.installationScript}
       '';
+
+      inputsFrom = with config; [
+        flake-root.devShell
+        treefmt.build.devShell
+        # FIXME: needed? https://zero-to-flakes.com/treefmt-nix/#add-treefmt-to-your-devshell
+        # treefmt.build.programs
+      ];
     };
   };
 }
