31f243e39eac9e954a337cb040b1c1333628e550
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-05 18:28:14 -0300

    feat: bootstrap reuse compliance

--- nix/flake/devShells/default.nix
+++ nix/flake/devShells/default.nix
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Lin Yinfeng <lin.yinfeng@outlook.com>
+# SPDX-FileCopyrightText: 2023 Unidealistic Raccoon <procyon@secureninja.maskmy.id>
+#
+# SPDX-License-Identifier: MIT
+
 { ... }: {
   perSystem = { self', config, pkgs, lib, ... }: {
     checks = lib.mapAttrs' (name: drv: lib.nameValuePair "devShells/${name}" drv) self'.devShells;
@@ -12,6 +17,11 @@
         treefmt.build.devShell
         # FIXME: needed? https://zero-to-flakes.com/treefmt-nix/#add-treefmt-to-your-devshell
         # treefmt.build.programs
+      ];
+
+      packages = with pkgs; [
+        # Legal
+        reuse
       ];
     };
   };
