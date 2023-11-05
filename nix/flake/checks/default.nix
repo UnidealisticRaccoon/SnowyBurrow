2b6d4e3c8ed433a6bad349d272f78e556df83ec8
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-05 09:52:48 -0300

    feat(checks): add pre-commit-hooks-nix - also fix README.md and dependabot.yml to comply with hook

--- nix/flake/checks/default.nix
+++ nix/flake/checks/default.nix
@@ -1,5 +1,25 @@
 { inputs, ... }: {
   imports = [
+    inputs.pre-commit-hooks-nix.flakeModule
     inputs.flat-flake.flakeModules.flatFlake
   ];
+
+  perSystem.pre-commit = {
+    check.enable = true;
+
+    settings = {
+      excludes = [
+        "flake.lock"
+      ];
+
+      hooks = {
+        nil.enable = true;
+        shfmt.enable = true;
+        prettier.enable = true;
+        shellcheck.enable = true;
+        commitizen.enable = true;
+        nixpkgs-fmt.enable = true;
+      };
+    };
+  };
 }
