4358c867286621199951289cb58315db27bf9850
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-14 16:44:06 -0300

    fix(reuse): add missing licenses

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023 Unidealistic Raccoon <procyon@secureninja.maskmy.id>
+#
+# SPDX-License-Identifier: MIT
+
 terraform {
   backend "local" {
     path = "terraform.tfstate.json"
