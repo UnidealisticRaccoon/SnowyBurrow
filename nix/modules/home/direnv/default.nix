fd00cfe00b2eb831ca2865daaa1ba784996d23d3
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-21 14:48:53 -0300

    feat(hm-module): add direnv aliases

--- nix/modules/home/direnv/default.nix
+++ nix/modules/home/direnv/default.nix
@@ -2,9 +2,40 @@
 #
 # SPDX-License-Identifier: MIT
 
+{ ... }:
 {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+  };
+
+  home.shellAliases = {
+    nixify = ''
+      if [ ! -e ./.envrc ]; then
+        echo "use nix" > .envrc
+        direnv allow
+      fi
+
+      if [[ ! -e shell.nix ]] && [[ ! -e default.nix ]]; then
+        cat > default.nix <<'EOF'
+        with import <nixpkgs> {};
+        mkShell {
+          nativeBuildInputs = [
+            bashInteractive
+          ];
+        }
+        EOF
+        ${"EDITOR:-nano"} default.nix
+      fi
+    '';
+    flakify = ''
+      if [ ! -e flake.nix ]; then
+        nix flake new -t github:nix-community/nix-direnv .
+      elif [ ! -e .envrc ]; then
+        echo "use flake" > .envrc
+        direnv allow
+      fi
+      ${"EDITOR:-nano"} flake.nix
+    '';
   };
 }
