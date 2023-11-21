23c39ff4795703f752d89379e4e39ccb2842a98d
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-21 14:37:50 -0300

    feat!: bootstrap @ehllie's ez-configs - migrate devcontainer home settings

---
+++ nix/users/vscode.nix
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Unidealistic Raccoon <procyon@secureninja.maskmy.id>
+#
+# SPDX-License-Identifier: MIT
+
+{ lib, ... }:
+{
+  home.username = lib.mkForce "vscode";
+}
