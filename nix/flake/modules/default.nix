bceef2887130bc3c1fee85a3c3c24d13654efd09
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-21 14:37:50 -0300

    feat!: bootstrap @ehllie's ez-configs - migrate devcontainer home settings

--- nix/flake/modules/default.nix
+++ nix/flake/modules/default.nix
@@ -1,14 +1,40 @@
 # SPDX-FileCopyrightText: 2023 Lin Yinfeng <lin.yinfeng@outlook.com>
 # SPDX-FileCopyrightText: 2023 Unidealistic Raccoon <procyon@secureninja.maskmy.id>
+# SPDX-FileCopyrightText: 2023 Elizabeth Paź <me@ehllie.xyz>
 #
 # SPDX-License-Identifier: MIT
 
-{ self, lib, ... }:
+{ self, inputs, lib, ... }:
 {
-  imports = lib.attrValues (import ../../modules/flake);
+  imports = [
+    inputs.ez-configs.flakeModule
+  ] ++ lib.attrValues (import ../../modules/flake);
 
-  flake = {
-    flakeModules = import ../../modules/flake;
-    homeModules = self.lib.rakeLeaves ../../modules/home;
+  flake.flakeModules = import ../../modules/flake;
+
+  ezConfigs = {
+    inherit (self.lib) globalArgs;
+
+    root = ./.;
+    nixos = {
+      modulesDirectory = ../../modules/nixos;
+      configurationsDirectory = ../../hosts/nixos;
+    };
+    darwin = {
+      modulesDirectory = ../../modules/darwin;
+      configurationsDirectory = ../../hosts/darwin;
+    };
+    home = {
+      modulesDirectory = ../../modules/home;
+      configurationsDirectory = ../../users;
+      users = {
+        vscode = {
+          standalone = {
+            enable = true;
+            pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+          };
+        };
+      };
+    };
   };
 }
