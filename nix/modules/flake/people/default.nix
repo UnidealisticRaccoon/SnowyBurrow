782aa13db10c368d4127671f86d409b59c62663f
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-23 12:53:24 -0300

    feat(hm-module): add git thanks to @srid

--- nix/modules/flake/people/default.nix
+++ nix/modules/flake/people/default.nix
@@ -5,16 +5,6 @@
 
 { lib, ... }:
 let
-  keySubmodule = lib.types.submodule {
-    options = {
-      gpg = lib.mkOption {
-        type = lib.types.str;
-      };
-      ssh = lib.mkOption {
-        type = lib.types.str;
-      };
-    };
-  };
   userSubmodule = lib.types.submodule {
     options = {
       name = lib.mkOption {
@@ -23,8 +13,13 @@ let
       email = lib.mkOption {
         type = lib.types.str;
       };
-      key = lib.mkOption {
-        type = lib.types.attrsOf keySubmodule;
+      keys = {
+        gpg = lib.mkOption {
+          type = lib.types.str;
+        };
+        ssh = lib.mkOption {
+          type = lib.types.str;
+        };
       };
     };
   };
@@ -57,7 +52,7 @@ in
       "${myself}" = {
         name = "Unidealistic Raccoon";
         email = "dw5pzgvhbglzdgljcmfjy29vbg@skiff.com";
-        key = {
+        keys = {
           gpg = "2764AA78791D69DF7E8916D640802D0919B2FB7D";
           ssh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLMCpAHL6U/68APRbekm/mzlBaRSNzi3GQzJYff0N69";
         };
