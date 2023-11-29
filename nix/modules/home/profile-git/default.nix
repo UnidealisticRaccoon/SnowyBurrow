4012eece68ae6dd237ffc7a6a11c9b77ebac70f9
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-11-29 11:47:20 -0300

    feat(hm-module): improve git aliases

--- nix/modules/home/profile-git/default.nix
+++ nix/modules/home/profile-git/default.nix
@@ -23,9 +23,16 @@
         signingkey = users.${myself}.keys.gpg;
       };
       alias = {
+        a = "add";
         co = "checkout";
         ci = "commit";
         cia = "commit --amend";
+        d = "diff";
+        l = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        plog = "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
+        tlog = "log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
+        rank = "shortlog -sn --no-merges";
+        rb = "rebase";
         st = "status";
         sw = "switch";
         b = "branch";
