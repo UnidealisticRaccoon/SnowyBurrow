f00355de16c48dd6a32079dfb35e6daa275ebac6
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-12-01 15:02:32 -0300

    feat(tf): add bastion to oci config

--- infrastructure/terraform/modules/oci/main.tf
+++ infrastructure/terraform/modules/oci/main.tf
@@ -10,7 +10,6 @@ resource "random_pet" "vcn_dns_label" {
 
 module "vcn" {
   source                   = "oracle-terraform-modules/vcn/oci"
-  compartment_id           = data.sops_file.oci.data["tenancy_ocid"]
   create_nat_gateway       = true
   create_service_gateway   = true
   create_internet_gateway  = true
@@ -18,27 +17,10 @@ module "vcn" {
   vcn_name                 = "Default VCN"
   vcn_cidrs                = ["${var.vcn_cidr}"]
   vcn_dns_label            = random_pet.vcn_dns_label.id
+  compartment_id           = data.sops_file.oci.data["tenancy_ocid"]
 }
 
-resource "oci_core_security_list" "tailscale" {
-  display_name   = "Tailscale"
-  vcn_id         = module.vcn.vcn_id
-  compartment_id = data.sops_file.oci.data["tenancy_ocid"]
-
-  ingress_security_rules {
-    protocol    = 17
-    stateless   = true
-    source      = "0.0.0.0/0"
-    description = "Tailscale IPv4 direct connections"
-
-    udp_options {
-      min = 41641
-      max = 41641
-    }
-  }
-}
-
-resource "random_pet" "public_dns_label" {
+resource "random_pet" "subnet_public_dns_label" {
   length = 1
 }
 
@@ -46,13 +28,13 @@ resource "oci_core_subnet" "public" {
   display_name      = "Public Subnet"
   vcn_id            = module.vcn.vcn_id
   route_table_id    = module.vcn.ig_route_id
-  dns_label         = random_pet.public_dns_label.id
   security_list_ids = [module.vcn.default_security_list_id]
+  dns_label         = random_pet.subnet_public_dns_label.id
   compartment_id    = data.sops_file.oci.data["tenancy_ocid"]
   cidr_block        = cidrsubnet(var.vcn_cidr, var.newbits["public"], var.netnum["public"])
 }
 
-resource "random_pet" "private_dns_label" {
+resource "random_pet" "subnet_private_dns_label" {
   length = 1
 }
 
@@ -60,8 +42,20 @@ resource "oci_core_subnet" "private" {
   display_name      = "Private Subnet"
   vcn_id            = module.vcn.vcn_id
   route_table_id    = module.vcn.nat_route_id
-  dns_label         = random_pet.private_dns_label.id
   security_list_ids = [module.vcn.default_security_list_id]
+  dns_label         = random_pet.subnet_private_dns_label.id
   compartment_id    = data.sops_file.oci.data["tenancy_ocid"]
   cidr_block        = cidrsubnet(var.vcn_cidr, var.newbits["private"], var.netnum["private"])
+}
+
+resource "random_pet" "main_bastion" {
+  length = 1
+}
+
+resource "oci_bastion_bastion" "main" {
+  bastion_type                 = "STANDARD"
+  client_cidr_block_allow_list = ["0.0.0.0/0"]
+  target_subnet_id             = oci_core_subnet.private.id
+  name                         = random_pet.main_bastion.id
+  compartment_id               = data.sops_file.oci.data["tenancy_ocid"]
 }
