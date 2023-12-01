d26e121691d062002c3c349d3c08f5d4ef1c5b94
Unidealistic Raccoon <dw5pzgvhbglzdgljcmfjy29vbg@skiff.com>
2023-12-01 15:00:10 -0300

    refactor: split tailscale files

---
+++ infrastructure/terraform/modules/tailscale/outputs.tf
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Unidealistic Raccoon <procyon@secureninja.maskmy.id>
+#
+# SPDX-License-Identifier: MIT
+
+output "tailnet_key-r1e0p1" {
+  sensitive = true
+  value     = tailscale_tailnet_key.r1e0p1.key
+}
