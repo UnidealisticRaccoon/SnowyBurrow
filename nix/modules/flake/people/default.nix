20d870710609c0790dd2103448c10a4c99681034
Unidealistic Raccoon <me@ursuslotor.xyz>
2023-12-09 13:18:44 -0300

    chore(flake-module): change default email

--- nix/modules/flake/people/default.nix
+++ nix/modules/flake/people/default.nix
@@ -51,7 +51,7 @@ in
     users = {
       "${myself}" = {
         name = "Unidealistic Raccoon";
-        email = "dw5pzgvhbglzdgljcmfjy29vbg@skiff.com";
+        email = "me@ursuslotor.xyz";
         keys = {
           gpg = "2764AA78791D69DF7E8916D640802D0919B2FB7D";
           ssh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLMCpAHL6U/68APRbekm/mzlBaRSNzi3GQzJYff0N69";
