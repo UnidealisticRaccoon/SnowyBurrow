f72ae3f8f95432d1ffc0e5f909d83bd1ad85ca68
Unidealistic Raccoon <me@ursuslotor.xyz>
2023-12-09 13:20:15 -0300

    chore(flake-module): add sshcontrol for gpg-agent

--- nix/modules/flake/people/default.nix
+++ nix/modules/flake/people/default.nix
@@ -17,6 +17,9 @@ let
         gpg = lib.mkOption {
           type = lib.types.str;
         };
+        sshcontrol = lib.mkOption {
+          type = lib.types.str;
+        };
         ssh = lib.mkOption {
           type = lib.types.str;
         };
@@ -54,6 +57,7 @@ in
         email = "me@ursuslotor.xyz";
         keys = {
           gpg = "2764AA78791D69DF7E8916D640802D0919B2FB7D";
+          sshcontrol = "6EE062284DC707753AFEA55664BE39978D8CFC29";
           ssh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLMCpAHL6U/68APRbekm/mzlBaRSNzi3GQzJYff0N69";
         };
       };
