ff04e7781b421aafaafa1b85eb53b0036b5456d5
Unidealistic Raccoon <me@ursuslotor.xyz>
2023-12-14 20:54:07 -0300

    feat(hm-module): improve ssh modularity

--- nix/modules/home/profile-ssh/default.nix
+++ nix/modules/home/profile-ssh/default.nix
@@ -2,12 +2,10 @@
 #
 # SPDX-License-Identifier: MIT
 
-{ config, lib, ... }:
+{ ... }:
 {
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      ${lib.optionalString config.services.gpg-agent.enable "IdentityAgent /run/user/1000/gnupg/S.gpg-agent.ssh \n"}
-    '';
+    includes = [ "config.d/*" ];
   };
 }
