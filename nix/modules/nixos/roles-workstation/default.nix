6f8acbdf9f7869bcbe7345c4894c3413b8577f74
Unidealistic Raccoon <me@ursuslotor.xyz>
2023-12-29 10:36:16 -0300

    feat(nixos-module): add workstation role

---
+++ nix/modules/nixos/roles-workstation/default.nix
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: 2023 Unidealistic Raccoon <procyon@secureninja.maskmy.id>
+#
+# SPDX-License-Identifier: MIT
+
+{ flake, ezModules, pkgs, lib, ... }:
+{
+  imports = [
+    ezModules.audio-pipewire
+    ezModules.profile-fonts
+    ezModules.profile-gnome
+    ezModules.profile-stylix
+  ];
+
+  documentation.nixos.enable = false;
+
+  networking.useDHCP = lib.mkDefault true;
+
+  hardware.enableRedistributableFirmware = true;
+
+  users.extraGroups.video.members = [ "${flake.config.people.myself}" ];
+
+  services = {
+    fwupd.enable = true;
+    xserver = {
+      enable = true;
+      excludePackages = [ pkgs.xterm ];
+      libinput.touchpad.naturalScrolling = true;
+    };
+  };
+}
