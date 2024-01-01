53ad822bc6d4c660bc41e0f0efd040d28f80faa6
Unidealistic Raccoon <me@ursuslotor.xyz>
2024-01-01 19:56:20 -0300

    chore(hm-modules): increase suite modularity

---
+++ nix/modules/home/suite-cli/default.nix
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2023 Unidealistic Raccoon <procyon@secureninja.maskmy.id>
+# SPDX-FileCopyrightText: 2023 Elizabeth Paź <me@ehllie.xyz>
+#
+# SPDX-License-Identifier: MIT
+
+{ ezModules, pkgs, lib, ... }:
+{
+  imports = [
+    ezModules.profile-direnv
+    ezModules.profile-git
+    ezModules.profile-gpg
+    ezModules.profile-nix-index
+    ezModules.profile-ssh
+    ezModules.profile-starship
+  ];
+}
