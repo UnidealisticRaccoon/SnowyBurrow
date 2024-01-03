b75b0ebd96934f9ab5a33e860b77ef285d5f7fa5
Unidealistic Raccoon <me@ursuslotor.xyz>
2024-01-03 11:27:18 -0300

    feat(hm-modules): add jq profile

--- nix/modules/home/suite-cli/default.nix
+++ nix/modules/home/suite-cli/default.nix
@@ -14,6 +14,7 @@
     ezModules.profile-gpg
     ezModules.profile-helix
     ezModules.profile-hyfetch
+    ezModules.profile-jq
     ezModules.profile-just
     ezModules.profile-nix-index
     ezModules.profile-pass
